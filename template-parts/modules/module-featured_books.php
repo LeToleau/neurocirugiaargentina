86b20c7fa53f1c19edcec4ed19ad41af1ce04f23
Olivero <oliverio@wcanvas>
2022-08-29 02:02:00 -0300

    14-set slider to featured books module

--- template-parts/modules/module-featured_books.php
+++ template-parts/modules/module-featured_books.php
@@ -4,40 +4,46 @@
     $featBooks = get_sub_field('featured_books');
 ?>
 
-<section class="m-featured-books">
+<section class="m-featured-books js-featured-books">
     <div class="m-featured-books__container container">
         <div class="m-featured-books__heading">
             <h2 class="m-featured-books__title"><?= esc_html($title); ?></h2>
             <p class="m-featured-books__copy"><?= esc_html($copy); ?></p>
         </div>
-        <div class="m-featured-books__books">
-            <?php
-                foreach($featBooks as $book) : 
-                $bookId = $book->ID;
-                $bookImage = get_field('book_preview_image', $bookId);
-                $bookAuthor = get_field('book_author', $bookId);
-                $bookDownload = get_field('book_download_link', $bookId);
-                $bookPreview = get_field('book_online_view_link', $bookId);
+        <div class="m-featured-books__books js-swiper-here swiper">
+            <div class="swiper-wrapper">
+                <?php
+                    foreach($featBooks as $book) : 
+                    $bookId = $book->ID;
+                    $bookImage = get_field('book_preview_image', $bookId);
+                    $bookAuthor = get_field('book_author', $bookId);
+                    $bookDownload = get_field('book_download_link', $bookId);
+                    $bookPreview = get_field('book_online_view_link', $bookId);
 
+                    ?>
+                        <div class="m-featured-books__card swiper-slide">
+                            <div class="m-featured-books__image-wrapper">
+                                <img src="<?php echo esc_url($bookImage['url']); ?>" alt="">
+                            </div>
+                            <div class="m-featured-books__text-container">
+                                <div class="m-featured-books__card-title">
+                                    <h4><?php echo $book->post_title; ?></h4>
+                                    <p><?php echo esc_html($bookAuthor); ?></p>
+                                </div>
+                                <div class="m-featured-books__buttons">
+                                    <a href="<?php echo esc_url($bookDownload['url']); ?>" class="button"><?php echo esc_html($bookDownload['title']); ?></a>
+                                    <a href="<?php echo esc_url($bookPreview['url']); ?>" class="button"><?php echo esc_html($bookPreview['title']); ?></a>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endforeach;
                 ?>
-                    <div class="m-featured-books__card">
-                        <div class="m-featured-books__image-wrapper">
-                            <img src="<?php echo esc_url($bookImage['url']); ?>" alt="">
-                        </div>
-                        <div class="m-featured-books__text-container">
-                            <div class="m-featured-books__card-title">
-                                <h4><?php echo $book->post_title; ?></h4>
-                                <p><?php echo esc_html($bookAuthor); ?></p>
-                            </div>
-                            <div class="m-featured-books__buttons">
-                                <a href="<?php echo esc_url($bookDownload['url']); ?>" class="button"><?php echo esc_html($bookDownload['title']); ?></a>
-                                <a href="<?php echo esc_url($bookPreview['url']); ?>" class="button"><?php echo esc_html($bookPreview['title']); ?></a>
-                            </div>
-                        </div>
-                    </div>
-                <?php endforeach;
-            ?>
+            </div>
         </div>
+
+        <button class='swiper-button-next js-swiper-button-next'></button>
+        <button class='swiper-button-prev js-swiper-button-prev'></button>
+        <div class='swiper-pagination js-video-swiper-pagination'></div>
 
         <div class="m-featured-books__cta-container">
             <a class="m-featured-books__cta button" href="<?php echo get_template_directory_uri(); ?>/libros">Explorar Todos</a>
